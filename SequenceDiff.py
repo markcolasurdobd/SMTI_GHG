3a61d336b166e21eeb9f4862e35f4e69a174c5a5
Mark Colasurdo <mark.colasurdo@bd.com>
2025-09-29 15:52:24 -0400

    progress on sequence matcher.

--- SequenceDiff.py
+++ SequenceDiff.py
@@ -1,13 +1,22 @@
 from difflib import SequenceMatcher
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Load data
+df23 = pd.read_csv('./data/ghg_filtered_2023.csv')
+df24 = pd.read_csv('./data/ghg_filtered_2024.csv')
 
+KEEP_24 = ['SEG', 'BU', 'Platform', 'Product Category', 'Product Line', 'Product Set', 'CAT 11 (USE)']
 
-text1 = "I like dogs"
+df24 = df24[KEEP_24]
 
-text2 = "I like dog"
+df24['string'] = df24['SEG'].str.cat(df24[df24.columns[1:-1]].astype(str), sep=' ')
 
-matcher = SequenceMatcher(None, text1, text2)
+seq1 = df24.string[1]
+seq2 = df24.string[2]
 
-ratio = matcher.ratio()
+ratios = [SequenceMatcher(None, seq1, seq).ratio() for seq in df24.string]
 
-print(ratio)--
+plt.hist(ratios)
+plt.show()
