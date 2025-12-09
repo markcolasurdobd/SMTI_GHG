5a95dd61a39cce427e8c1a621c5cd8742dda5890
Mark Colasurdo <mark.colasurdo@bd.com>
2025-12-09 09:21:04 -0500

    Some cat 11 and 12 validation

--- src/scripts/validation_script.py
+++ src/scripts/validation_script.py
@@ -1,27 +1,33 @@
+from src.data import TrainData
 import pandas as pd
 import src.training as train
 import src.predicting as pred
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-# Load data
-df_train =  pd.read_csv('./data/train.csv')
-df_val = pd.read_csv('./data/validation.csv')
+# Load and preprocess data
+df =  pd.read_csv('./data/2024_w_cat12.csv')
+exclude_rows = df[' COMMENTS '].str.contains('exclude', case = False, na = False)
+df_included = df[~exclude_rows]
+d24 = TrainData()
+X_cols = ['SEG', 'BU', 'Platform', 'Product Category', 'Product Line', 'Product Set']
+y_col = ['CAT 11 (USE)']
+d24.df = df_included[X_cols + y_col]
+d24.transform()
 
 # Split df into X and y
-X_train, y_train = df_train.iloc[:, 0], df_train.iloc[:, -1]
-X_val, y_val = df_val.iloc[:, 0], df_val.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(d24.X, d24.y, test_size=0.2, random_state=42)
 
 # Train the model
 model, vectorizer = train.train_model(X_train, y_train)
 
 # Predict on X_val
-y_preds = pred.predict(model, vectorizer, X_val)
+y_preds = pred.predict(model, vectorizer, X_test)
 
 # Run classification report
-cr = classification_report(y_val, y_preds, output_dict=False)
-print(cr)
+cr = classification_report(y_test, y_preds.preds, output_dict=True)
+#print(cr)
 
-# Save classification report
-cr = classification_report(y_val, y_preds, output_dict=True)
-df_cr = pd.DataFrame(cr).transpose()
-df_cr.to_csv('./results/validation_results_21_24_unbalanced.csv')
+# Save results
+df_report = pd.DataFrame(cr).transpose()
+df_report.to_csv('./results/2024_cat11_report.csv')
