4c01f080d6fd7cddc6838d5cecf6acd1eac38a5f
Mark Colasurdo <mark.colasurdo@bd.com>
2025-12-11 13:54:09 -0500

    latest

--- src/scripts/match_and_model_simultaneous.py
+++ src/scripts/match_and_model_simultaneous.py
@@ -2,61 +2,98 @@ import pandas as pd
 from src.data import PredictData
 import src.predicting as pred
 
-# Load data
+# Some paths at the top
 ref_path = r"./data/2024.csv"
-tar_path = r"./data/2025.csv"
+tar_path = r"./data/2025_source.csv"
+model_path_cat11 = r"C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models\model_24_cat11_ff.pkl"
+model_path_cat12 = r"C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models\model_24_cat12_ff.pkl"
+vec_path_cat11 = r"C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models\vec_24_cat11_ff.pkl"
+vec_path_cat12 = r"C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models\vec_24_cat12_ff.pkl"
+results_path = r'./results/2025_results_ff+source+24exc.csv'
+
+# Columns for categorization
+X_columns = ['SEG', 'BU', 'Business Unit', 'Sub BU', 'Sub Business Unit', 'Platform',
+                'Product Category ID', 'Product Category', 'Product Line ID',
+                'Product Set ID', 'Platform ID', 'Product Line', 'Product Set',
+                'Material Description', 'Product Subset ID', 'Product Subset']
+
+# Load data
 ref = pd.read_csv(ref_path)
 tar = pd.read_csv(tar_path)
 
-# Add cat 11 and 12 columns to target df
+# Create variables for columns
 cat11 = 'CAT 11 (USE)'
 cat12 = 'CAT 12 (EoL)'
+matid = 'Material ID'
+comm = ' COMMENTS '
+unit = ' FY 25 Actual Units '
 
-# Label category excluded based on comments
-tar.loc[~tar[' COMMENTS '].isna(), [cat11, cat12]] = 'excluded'
+# Label category excluded based on comments in the ref data
+#tar.loc[tar[comm].str.contains('exclude', case = False), [cat11, cat12]] = 'excluded'
+
+# Remove rows that contain 'excluded' in comments, cat11, and cat12 from ref data
+ref[comm] = ref[comm].astype(str)
+# ref = ref[~ref[comm].str.contains('exclude', case = False)]
+# ref = ref[~ref[cat11].str.contains('exclude', case = False)]
+# ref = ref[~ref[cat12].str.contains('exclude', case = False)]
+ref[cat11] = ref[cat11].str.lower()
+ref[cat12] = ref[cat12].str.lower()
 
 # Now, branch off the subset of rows that are not excluded
-tar_inc = tar[tar[cat11].isna()]
+#tar[comm] = tar[comm].astype(str)
+#tar_inc = tar[~tar[comm].str.contains('exclude', case = False)]
+tar_inc = tar
 
 # Convert Material ID column to string
-tar_inc['Material ID'] = tar_inc['Material ID'].astype(str)
-ref['Material ID'] = ref['Material ID'].astype(str)
-ref.drop_duplicates(subset=['Material ID'], inplace = True)
+tar_inc[matid] = tar_inc[matid].astype(str)
+ref[matid] = ref[matid].astype(str)
+ref.drop_duplicates(subset=matid, inplace = True)
 
 # Match/merge on Material ID
-match = tar_inc.merge(ref[['Material ID', cat11, cat12]], on = 'Material ID', how = 'left')
-tar_inc[[cat11, cat12]] = match[[cat11 + '_y', cat12 + '_y']].values
+match = tar_inc.merge(ref[[matid, cat11, cat12]], on = matid, how = 'left')
+tar_inc[[cat11, cat12]] = match[[cat11, cat12]].values
+tar_inc.loc[~tar_inc[cat11].isna(), comm] = 'Matched by Material ID'
 
 # Now, branch off the remaining, unmatched rows for machine learning
 tar_unmatched = tar_inc[tar_inc[cat11].isna()]
 
-# Start here
-
 # Classify unmatched rows based on trained model
-d25 = PredictData()
-X_columns = ['SEG', 'BU', 'Platform', 'Product Category', 'Product Line', 'Product Set']
-d25.df = df25_unmatched
-d25.transform()
-]
+tar_data = PredictData()
+tar_data.df = tar_unmatched[X_columns]
+tar_data.transform()
+
 # Load models for inference
-# Load model
-model_path = r"C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models\model_24_cat12.pkl"
-model = pred.load_model(model_path)
+model_cat11 = pred.load_model(model_path_cat11)
+model_cat12 = pred.load_model(model_path_cat12)
 
 # Load vectorizer
-vec_path = r"C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models\vec_24_cat12.pkl"
-vectorizer = pred.load_vectorizer(vec_path)
+vec_cat11 = pred.load_vectorizer(vec_path_cat11)
+vec_cat12 = pred.load_vectorizer(vec_path_cat12)
 
 # Run prediction
-preds = pred.predict(model, vectorizer, d25.X)
-df25_unmatched[cat] = preds.preds.values
+preds_cat11 = pred.predict(model_cat11, vec_cat11, tar_data.X)
+preds_cat12 = pred.predict(model_cat12, vec_cat12, tar_data.X)
+tar_unmatched[cat11] = preds_cat11.preds.values
+tar_unmatched[cat12] = preds_cat12.preds.values
+tar_unmatched.loc[~tar_unmatched[cat11].isna(), comm] = 'Classified by ML model'
 
 # Relabel primary df
-df25.loc[df25_inc.index, cat] = df25_inc[cat]
-df25.loc[df25_inc.index, ' COMMENTS '] = 'Matched by Material ID'
-df25.loc[df25_unmatched.index, cat] = df25_unmatched[cat]
-df25.loc[df25_unmatched.index, ' COMMENTS '] = 'Classified by ML model'
-df25[cat] = df25[cat].str.lower()
+tar.iloc[tar_inc.index] = tar_inc
+tar.iloc[tar_unmatched.index] = tar_unmatched
+
+# Check duplicate Material IDs in unmatched data
+matid_dup = tar.loc[tar[matid].duplicated(), matid].unique()
 
 # Export results
-df25.to_csv('./results/2025_cat12_results_2.csv')
+tar.to_csv(results_path)
+
+
+
+
+
+# units = tar[unit]
+# units = units.str.strip()
+# units[units.str.contains('-')] = '0'
+# units = units.str.strip('(')
+# units = units.str.strip(')')
+# units = units.str.replace(',', '')
--- src/scripts/training_script.py
+++ src/scripts/training_script.py
@@ -2,17 +2,28 @@ import pandas as pd
 from src.data import TrainData
 import src.training as tr
 
+# To Do
+# Retrain algorithm including product subset id, run on source 25 data, send to di.
+# Compare some metrics to current algorithm
+
+# Define which category you want to train for
+#cat = 'CAT 11 (USE)'
+cat = 'CAT 12 (EoL)'
+
 # Load and preprocess data
 d24 = TrainData()
-d24.load_csv('./data/2024_w_cat12.csv')
-keep_columns = ['SEG', 'BU', 'Platform', 'Product Category', 'Product Line', 'Product Set', 'CAT 12 (EoL)']
+d24.load_csv('./data/2024.csv')
+
+# Remove excluded data and down-select columns for training
+d24.df = d24.df[~d24.df[' COMMENTS '].str.contains('exclude', case = False, na = False)]
+d24.df = d24.df[~d24.df['CAT 11 (USE)'].str.contains('exclude', case = False, na = False)]
+d24.df = d24.df[~d24.df['CAT 12 (EoL)'].str.contains('exclude', case = False, na = False)]
+keep_columns = ['SEG', 'BU', 'Business Unit', 'Sub BU', 'Sub Business Unit', 'Platform',
+                'Product Category ID', 'Product Category', 'Product Line ID',
+                'Product Set ID', 'Platform ID', 'Product Line', 'Product Set',
+                'Material Description', 'Product Subset ID', 'Product Subset', cat]
 d24.df = d24.df[keep_columns]
 d24.transform()
-d24.remove_value('blank', d24.X)
-d24.replace_substring('exclude')
-exclude = d24.y[d24.y == 'exclude'].index
-d24.X = d24.X.drop(exclude)
-d24.y = d24.y.drop(exclude)
 X = d24.X
 y = d24.y
 
@@ -21,10 +32,10 @@ model, vectorizer = tr.train_model(X, y)
 
 # Save model
 output_dir = r'C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models'
-model_name = 'model_24_cat12.pkl'
+model_name = 'model_24_cat12_ff.pkl'
 tr.save_model(model, output_dir=output_dir, model_name=model_name)
 
 # Save vectorizer
 output_dir = r'C:\Users\10354191\OneDrive - BD\Projects\SMTI\GHG\models'
-vectorizer_name = 'vec_24_cat12.pkl'
+vectorizer_name = 'vec_24_cat12_ff.pkl'
 tr.save_vectorizer(vectorizer, output_dir=output_dir, vectorizer_name=vectorizer_name)
